855ab5b2521c04661c14900681a0a146e0c81eed
Vy Nguyen <vy.t.nguyen.11@gmail.com>
2018-02-09 17:55:27 -0800

    Ready for deploy

---
+++ send-sms9302.js
@@ -0,0 +1,13 @@
+const accountSid = 'AC12f809c0f491bb22d9de11e0ae1f8f36';
+const authToken = '6a0a88c5b79189e2e9b14d2ccf9f64ea';
+
+const client = require('twilio')(accountSid, authToken);
+
+client.messages.create ({
+to: '16194848576',
+from: '16195865063',
+body: 'Hello Vy, do not forget to go to Monkey King at 8pm tonight' 
+
+})
+
+.then((message) => console.log(message.sid));
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+var profile = require("./profile");
+const accountSid = "AC12f809c0f491bb22d9de11e0ae1f8f36";
+const authToken = "6a0a88c5b79189e2e9b14d2ccf9f64ea";
+
+const client = require("twilio")(accountSid, authToken);
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/profile", profile);
+
+app.set("views", "./views");
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.post("/thanks", (req, res) => {
+  console.log(accountSid, authToken);
+  console.log(req.body);
+  client.messages
+    .create({
+      to: "16194848576",
+      from: "16195865063",
+      body: `${req.body.name} (${req.body.email}) - ${req.body.subject}`
+    })
+    .then(message => {
+      console.log('this route');
+      console.log(message.sid);
+      res.render("thanks", { contact: req.body });
+    })
+    .catch(err => console.log(err));
+});
+
+app.listen(8080, () => {
+  console.log("listening at http://localhost:8080");
+});
