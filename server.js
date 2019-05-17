f0292ec897c13a608efbeb908948629d1f1d7ec0
Vy Nguyen <vy.t.nguyen.11@gmail.com>
2019-05-17 16:44:57 -0700

    d

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 var profile = require("./profile");
-const accountSid = "AC12f809c0f491bb22d9de11e0ae1f8f36";
+const accountSid = "AC12f809c0f491bb22d9de11e0ae1f8f";
 const authToken = "6a0a88c5b79189e2e9b14d2ccf9f64ea";
 
 const client = require("twilio")(accountSid, authToken);
@@ -37,7 +37,7 @@ app.post("/thanks", (req, res) => {
   client.messages
     .create({
       to: "16194848576",
-      from: "16195865063",
+      from: "6193050958",
       body: `${req.body.name} (${req.body.email}) - ${req.body.subject}`
     })
     .then(message => {
